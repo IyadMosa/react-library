8958d471aee68abfc19fef784df1b5413000ce20
IyadA <iyad_mosa@hotmail.com>
2022-12-17 12:46:35 +0200

    add Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from "react";
+import {
+  ButtonsWrapper,
+  Container,
+  ContentsWrapper,
+  HeaderWrapper,
+  TitleWrapper,
+  XWrapper,
+} from "./style";
+
+export interface ModalProps {
+  title?: string;
+  children?: any;
+  onClose?: any;
+  onSubmit?: any;
+  isShowButtons?: boolean;
+}
+
+const Modal: FC<ModalProps> = ({
+  children,
+  onClose = () => 0,
+  onSubmit = () => 0,
+  isShowButtons = true,
+  title = "",
+}) => {
+  console.log("modal");
+  return (
+    <Container className={"modal"}>
+      <HeaderWrapper>
+        <TitleWrapper>{title}</TitleWrapper>
+        <XWrapper>
+          <span onClick={onClose}>X</span>
+        </XWrapper>
+      </HeaderWrapper>
+      <ContentsWrapper>{children}</ContentsWrapper>
+      {isShowButtons && (
+        <ButtonsWrapper>
+          <button className="btn btn-success" onClick={onSubmit}>
+            {"Submit"}
+          </button>
+          <button className="btn btn-primary" onClick={onClose}>
+            {"Close"}
+          </button>
+        </ButtonsWrapper>
+      )}
+    </Container>
+  );
+};
+export default Modal;
---
+++ src/components/Modal/style.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: fixed;
+  box-shadow: 1px 1px 10px rgba(85, 85, 85, 0.15);
+  background: #ffffff;
+  border-radius: 5px;
+  height: 100%;
+  width: 100%;
+  top: 10%;
+  left: 30%;
+  max-width: 800px;
+  max-height: 500px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleWrapper = styled.div`
+  font-family: "Titillium Web";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  text-transform: capitalize;
+  color: #3c4144;
+`;
+
+export const XWrapper = styled.div`
+  font-family: "Titillium Web";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+  align-items: center;
+  text-transform: capitalize;
+  color: #3c4144;
+  display: flex;
+  justify-content: flex-end;
+`;
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 40px;
+  padding: 20px;
+`;
+
+export const ContentsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+  gap: 50px;
+  max-height: 90%;
+  min-height: 80%;
+  border: 1px solid beige;
+  overflow: overlay;
+`;
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  height: 60px;
+  padding: 5px;
+  gap: 15px;
+`;
