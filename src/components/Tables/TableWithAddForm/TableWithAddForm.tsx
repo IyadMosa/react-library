239b6ecfe39b948d2a39763de9391a6ae11a0f42
IyadA <iyad_mosa@hotmail.com>
2022-12-23 09:02:13 +0200

    update the table with add to use the add form

--- src/components/Tables/TableWithAddForm/TableWithAddForm.tsx
+++ src/components/Tables/TableWithAddForm/TableWithAddForm.tsx
@@ -1,9 +1,7 @@
-import React, { FC, useState } from "react";
-import IconButton from "@material-ui/core/IconButton";
-import AddCircleIcon from "@material-ui/icons/AddCircle";
+import React, { FC } from "react";
 import { Column } from "react-table";
 import Table from "../Table/Table";
-import Modal from "../../Modal";
+import { AddForm } from "../../AddEditForm";
 
 export interface TableWithFormProps {
   tableTitle: string;
@@ -24,38 +22,15 @@ const TableWithAddForm: FC<TableWithFormProps> = ({
   columns,
   ...props
 }) => {
-  const [showAddModel, setShowAddModel] = useState(false);
   return (
     <div>
       <Table
         tableTitle={tableTitle}
         data={data}
         columns={columns}
-        addComponent={
-          <IconButton color="primary" onClick={() => setShowAddModel(true)}>
-            <AddCircleIcon />
-          </IconButton>
-        }
+        addComponent={<AddForm />}
         minWidth={props.minWidth}
       />
-
-      {showAddModel && (
-        <div>
-          <Modal
-            onClose={() => {
-              setShowAddModel(false);
-            }}
-            isShowButtons={true}
-            title={props.modelTitle}
-            onSubmit={() => {
-              props.onAddSubmit();
-              setShowAddModel(false);
-            }}
-          >
-            {props.addForm}
-          </Modal>
-        </div>
-      )}
     </div>
   );
 };
