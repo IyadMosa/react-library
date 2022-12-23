dfad601988d8b4476c67bc4c9d9165e3913594c4
IyadA <iyad_mosa@hotmail.com>
2022-12-23 09:03:38 +0200

    add new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,6 @@ export { default as TextFieldRange } from "./TextFields/TextFieldRange";
 export { default as Modal } from "./Modal";
 export { default as Table } from "./Tables/Table";
 export { default as TableWithAddForm } from "./Tables/TableWithAddForm";
+export * from "./AddEditForm/AddEditSearchForm";
+export { default as Dropdown } from "./Dropdowns/Dropdown";
+export { default as DropdownWithAddForm } from "./Dropdowns/DropdownWithAdd";
